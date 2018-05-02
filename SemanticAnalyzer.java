a0a925da0187c5da70fd72b4e728ef3b869e6e3d
dolfander <daniel.pmachado@hotmail.com>
2018-05-02 02:01:16 +0100

    assign semantic

--- SemanticAnalyzer.java
+++ SemanticAnalyzer.java
@@ -54,9 +54,6 @@ public class SemanticAnalyzer {
      */
     public static boolean validateFunction(SymbolTable t, SimpleNode nd)throws ParseException{
 
-
-
-
         String call_name = nd.getName();
         ASTArgumentList call;
         //TODO funcoes nao feitas por mim
@@ -124,7 +121,7 @@ public class SemanticAnalyzer {
         }
     }
 
-    //
+
 
 
     /*
@@ -166,82 +163,92 @@ public class SemanticAnalyzer {
         return ret;
     }
 
-    //
-    // public void compStructureAssignment(){
-    //
-    // }
-    //
-    //
-    //
-    //
-    // /*
-    //  *  Checks the assignment of a scalar
-    //  */
-    // public String nonIntegerAssignment(Node node, SymbolTable t){
-    //
-    //     SimpleNode nn = (SimpleNode)node;
-    //     String name = nn.getName();
-    //
-    //     if(nn.children==null && checkVarExists(t, name) && checkVarInitialized(t,name)){   //a=N
-    //
-    //         if(t.getTypeOfSymbol(name).equals("Array")){
-    //             return "Array";
-    //         }
-    //         else if( checkVarScalar(t, name) && checkVarInitialized(t,name)){
-    //             return "Scalar";
-    //         }
-    //     }
-    //
-    //     else if(nn.children!=null){
-    //
-    //         SimpleNode nd = (SimpleNode)nn.children[0];
-    //         name = nd.getName();
-    //
-    //         if(nd instanceof ASTArrayAccess ){  //a=N[x]
-    //
-    //             if(! ( checkVarExists(t, name) && checkVarInitialized(t, name) &&checkVarArray(t, name))) return null;
-    //
-    //             nd = (SimpleNode)nd.children[0];
-    //             name = nd.getName();
-    //
-    //             if(nd instanceof ASTIndexID ){  //a=N[b]
-    //
-    //                 if( checkVarExists(t, name) && checkVarScalar(t, name)&& checkVarInitialized(t,name)){
-    //                     return "Scalar";
-    //                 }
-    //                 else {
-    //                     return null;
-    //                 }
-    //             }
-    //
-    //             else if(nd instanceof ASTIndexInteger){   //a=N[0]
-    //                 return "Scalar";
-    //             }
-    //         }
-    //
-    //         else if(nd instanceof ASTScalarAccess ){ //a=N.size
-    //
-    //             if(! ( checkVarExists(t, name) && checkVarArray(t, name))) return null;
-    //             return "Scalar";
-    //         }
-    //
-    //         else if(nd instanceof ASTFunctionTermAccess){   //a=N.b()
-    //             return "Scalar";
-    //         }
-    //
-    //         else if(nd instanceof ASTCall){  //a=N()
-    //
-    //             if(validateFunction(t, nd)){
-    //                 String type = t.getRetType(nd.getName());
-    //                 if(type.equals("void")){
-    //                     System.out.println("Semantic error - Assigning a variable to a void function");
-    //                     return null;
-    //                 }
-    //                 return type;
-    //             }
-    //             else return null;
-    //         }
-    //     }
-    //     return null;
-    // }
+
+    public void compStructureAssignment(){
+
+    }
+
+
+
+
+    /*
+     *  Checks the assignment of a scalar
+     */
+    public String nonIntegerAssignment(Node node, SymbolTable t){
+
+        SimpleNode nn = (SimpleNode)node;
+        String name = nn.getName();
+        String func_name = nn.getFuncName();
+        int line = nn.getToken().beginLine;
+
+        if(nn.children==null && checkVarExists(t, name, func_name, line) && checkVarInitialized(t,name, func_name)){   //a=N
+
+            if(t.getTypeVariable(func_name, name).equals("ARRAY")){
+                return "Array";
+            }
+            else if( checkVarScalar(t, name, func_name) && checkVarInitialized(t,name, func_name)){
+                return "Scalar";
+            }
+        }
+
+        else if(nn.children!=null){
+
+            SimpleNode nd = (SimpleNode)nn.children[0];
+            name = nd.getName();
+            func_name = nd.getFuncName();
+            line = nd.getToken().beginLine;
+
+            if(nd instanceof ASTArrayAccess ){  //a=N[x]
+
+                if(! ( checkVarExists(t, name, func_name, line) && checkVarInitialized(t, name, func_name) &&checkVarArray(t, name, func_name))) return null;
+
+                nd = (SimpleNode)nd.children[0];
+                name = nd.getName();
+                func_name = nd.getFuncName();
+                line = nd.getToken().beginLine;
+
+                if(nd instanceof ASTIndex ){  //a=N[b]
+
+                    if( checkVarExists(t, name, func_name, line) && checkVarScalar(t, name, func_name)&& checkVarInitialized(t,name, func_name)){
+                        return "Scalar";
+                    }
+                    else {
+                        return null;
+                    }
+                }
+
+                else if(nd instanceof ASTArraySize){   //a=N[0]
+                    return "Scalar";
+                }
+            }
+
+            else if(nd instanceof ASTScalarAccess ){ //a=N.size
+
+                if(! ( checkVarExists(t, name, func_name, line) && checkVarArray(t, name, func_name))) return null;
+                return "Scalar";
+            }
+
+            else if(nd instanceof ASTFunction){   //a=N.b()
+                return "Scalar";
+            }
+
+            else if(nd instanceof ASTCall){  //a=N()
+
+                try{
+                  if(validateFunction(t, nd)){
+                      String type = t.getRetType(nd.getName());
+                      if(type.equals("void")){
+                          System.out.println("Semantic error - Assigning a variable to a void function");
+                          return null;
+                      }
+                      return type;
+                  }
+                }
+                catch(Exception e){
+                  return null;
+                }
+            }
+        }
+        return null;
+    }
 }
